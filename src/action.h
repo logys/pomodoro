60e64a6ec59d6437b188af12e4cba3de99dd63f3
Luis Antonio Sánchez Monterde <antonio.dgr.ink@gmail.com>
2020-01-22 14:33:15 -0600

    Head

---
+++ src/action.h
@@ -0,0 +1,5 @@
+#ifndef ACTION_H
+#define ACTION_H
+
+
+#endif // ACTION_H
---
+++ test/test_action.c
@@ -0,0 +1,16 @@
+#include "unity.h"
+
+#include "action.h"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_init_actions(void)
+{
+
+}
