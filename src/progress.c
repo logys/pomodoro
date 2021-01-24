77dcd698f346be4e3592b293e5850832c819406f
Luis Antonio Sánchez Monterde <antonio.dgr.ink@gmail.com>
2021-01-23 21:07:17 -0600

    cleaned progress

--- src/progress.c
+++ src/progress.c
@@ -1,6 +1,8 @@
 #include "progress.h"
 int progress_update(const Session session)
 {
-	return 100*(1-(session.session_time - session.acumulated_time)/
-		       session.session_time);
+	double proportion  = (session.session_time - session.acumulated_time)
+		/ session.session_time;
+	double progress = 1 - proportion;
+	return 100*(progress);
 }
--- test/test_progress.c
+++ test/test_progress.c
@@ -1,10 +1,8 @@
 #include "unity.h"
-
 #include "../src/progress.h"
-#include "../src/pomodoro.h"
-#include "spys/millis_spy.h"
+#include "../src/session.h"
 
-Pomodoro pomodoro;
+Session session;
 void setUp(void)
 {
 }
@@ -15,18 +13,16 @@ void tearDown(void)
 
 void test_calculate_progress_at_init(void)
 {
-	pomodoro.session.session_time = 5;
-	pomodoro.session.acumulated_time = 0;
+	session.session_time = 5;
+	session.acumulated_time = 0;
 
-	pomodoro.progress = progress_update(pomodoro.session);
-	TEST_ASSERT_EQUAL(0, pomodoro.progress);
+	TEST_ASSERT_EQUAL(0, progress_update(session));
 }
 
 void test_calculate_progress_at_fiven_time_for_five_minutes(void)
 {
-	pomodoro.session.session_time = 5;
-	pomodoro.session.acumulated_time = 2.5;
+	session.session_time = 5;
+	session.acumulated_time = 2.5;
 
-	pomodoro.progress = progress_update(pomodoro.session);
-	TEST_ASSERT_EQUAL(50, pomodoro.progress);
+	TEST_ASSERT_EQUAL(50, progress_update(session));
 }
