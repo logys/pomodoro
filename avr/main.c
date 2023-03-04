eac218347fca06c2038294b22cc4c61dc7b3d856
Luis Antonio <antonio.dgr.ink@gmail.com>
2023-03-04 12:29:03 -0600

    128khz config

--- avr/main.c
+++ avr/main.c
@@ -1,11 +1,13 @@
 #include "tasks/input.h"
 #include "tasks/pomodoro.h"
 #include "hal/hal.h"
+#include <avr/io.h>
 
 #define VERSION 0.8.0
 
 int main()
 {
+	ADCSRA &= ~(1<<ADEN);
 	input_init();
 	const int session_time = 20;
 	pomodoro_init(session_time);
--- avr/tick.c
+++ avr/tick.c
@@ -4,10 +4,10 @@
 void tick_init(void)
 {
 	TCCR0A = (1U << WGM01) | (0U << WGM00);
-    	TCCR0B = (0U << CS02 ) | (1U << CS01) | (1U << CS00); /* 1/2^10 */
+    	TCCR0B = (0U << CS02 ) | (1U << CS01) | (0U << CS00); // 128k/8
     	TIMSK = (1U << OCIE0A); /* Enable TIMER2 compare Interrupt */
     	TCNT0 = 0U;
-    	OCR0A = 156;
+    	OCR0A = 159; //160 ticks for 10ms interrupt
 }
 
 ISR(TIMER0_COMPA_vect) 
