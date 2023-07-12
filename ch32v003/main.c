f0c5176584b5c54b3bb0ce05f5f0547b8aa77e88
Luis Antonio <antonio.dgr.ink@gmail.com>
2023-07-11 20:28:23 -0600

    blink ch32v003

--- ch32v003/main.c
+++ ch32v003/main.c
@@ -1,9 +1,30 @@
+#define SYSTEM_CORE_CLOCK 48000000UL
 
 #define VERSION 0.8.0
+#include "ch32v003fun.h"
 
-int main()
+#define APB_CLOCK SYSTEM_CORE_CLOCK 
+
+int main(void)
 {
+	uint32_t count=0;
+	SystemInit48HSI();
+	RCC->APB2PCENR |= RCC_APB2Periph_GPIOD | RCC_APB2Periph_GPIOC;
+	GPIOD->CFGLR &= ~(0xf<<(4*0));
+	GPIOD->CFGLR |= (GPIO_Speed_10MHz | GPIO_CNF_OUT_PP)<<(4*0);
+	GPIOD->CFGLR &= ~(0xf<<(4*4));
+	GPIOD->CFGLR |= (GPIO_Speed_10MHz | GPIO_CNF_OUT_PP)<<(4*4);
+	GPIOC->CFGLR &= ~(0xf<<(4*0));
+	GPIOC->CFGLR |= (GPIO_Speed_10MHz | GPIO_CNF_OUT_PP)<<(4*0);
 	while(1){
+		GPIOD->BSHR = 1 | (1<<4);        // Turn on GPIOs
+                GPIOC->BSHR = 1;
+                Delay_Ms( 250 );
+                GPIOD->BSHR = (1<<16) | (1<<(16+4)); // Turn off GPIODs
+                GPIOC->BSHR = (1<<16);
+                Delay_Ms( 250 );
+                count++;
+
 	}
 	return 0;
 }
--- cmake/ch32v003.cmake
+++ cmake/ch32v003.cmake
@@ -1,21 +1,20 @@
 set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR "riscv")
 
-set(CMAKE_C_COMPILER /home/antonio/Builds/riscv32-unknown-elf.gcc-12.1.0/bin/riscv32-unknown-elf-gcc)
+set(CMAKE_C_COMPILER /home/antonio/Builds/riscv32-unknown-elf.gcc-10.1.0.rv32e.ilp32e.newlib/bin/riscv32-unknown-elf-gcc)
 
 set(CMAKE_C_FLAGS 
-	"-g -Wall -Os -Wextra -pipe\
+	"-g -Os -flto -ffunction-sections\
 	-static-libgcc -lgcc\
-	-march=rv32i \
-	-mabi=ilp32 \
+	-march=rv32ec \
+	-mabi=ilp32e \
 	-DTINYVECTOR\
-	-ffunction-sections\
+	-nostdlib\
 	"
 	CACHE INTERNAL "C Compiler Flags")
 set(CMAKE_EXE_LINKER_FLAGS
-	"-Wl,--gc-sections -nostartfiles --static\
-	-specs=nano.specs -specs=nosys.specs\
-	"
+	"-Wl,--gc-sections\
+	-T${PROJECT_SOURCE_DIR}/cmake/ch32v003fun.ld"
 	CACHE INTERNAL "Linker options")
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
