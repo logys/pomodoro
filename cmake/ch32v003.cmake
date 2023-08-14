5ddcf297e086e3129ac8838aba74e01bfc48f84d
Luis Antonio <antonio.dgr.ink@gmail.com>
2023-08-13 22:21:52 -0600

    flto configured

--- cmake/ch32v003.cmake
+++ cmake/ch32v003.cmake
@@ -1,20 +1,20 @@
 set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR "riscv")
 
-#set(CMAKE_C_COMPILER /home/antonio/Builds/riscv32-unknown-elf.gcc-10.1.0.rv32e.ilp32e.newlib/bin/riscv32-unknown-elf-gcc)
 set(CMAKE_C_COMPILER /home/antonio/Builds/riscv/bin/riscv64-unknown-elf-gcc)
 
 set(CMAKE_C_FLAGS 
-	"-g -Os -flto -ffunction-sections\
+	"-g -Os -ffunction-sections\
 	-static-libgcc -lgcc\
 	-march=rv32ec_zicsr \
 	-mabi=ilp32e \
 	-DTINYVECTOR\
 	-nostdlib\
+	-flto -ffat-lto-objects\
 	"
 	CACHE INTERNAL "C Compiler Flags")
 set(CMAKE_EXE_LINKER_FLAGS
-	"-Wl,--gc-sections\
+	"-Wl,--gc-sections -flto -Wl,-flto\
 	-T${PROJECT_SOURCE_DIR}/ch32v003/ch32v003fun.ld"
 	CACHE INTERNAL "Linker options")
 
