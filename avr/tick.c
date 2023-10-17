3bbdd13ba82d9ded6fc7caa8cd9a0611296a457d
Luis Antonio <antonio.dgr.ink@gmail.com>
2023-10-16 21:51:21 -0600

    Avr pomodoro working

--- avr/tick.c
+++ avr/tick.c
@@ -3,7 +3,9 @@
 
 void tick_init(void)
 {
+	/* Cleat time on conpare configured */
 	TCCR0A = (1U << WGM01) | (0U << WGM00);
+	/* prescaler 8 128k/8 => 16k*/
     	TCCR0B = (0U << CS02 ) | (1U << CS01) | (0U << CS00); // 128k/8
     	TIMSK = (1U << OCIE0A); /* Enable TIMER2 compare Interrupt */
     	TCNT0 = 0U;
